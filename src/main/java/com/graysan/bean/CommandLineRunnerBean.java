72b2434863583fd59e8d8d3d3bd5db8b61a4cf9f
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-08-21 11:25:13 +0300

    CommandLineRunnerBean

---
+++ src/main/java/com/graysan/bean/CommandLineRunnerBean.java
@@ -0,0 +1,17 @@
+package com.graysan.bean;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Log4j2
+public class CommandLineRunnerBean {
+
+    public CommandLineRunner commandLineRunnerMethod(){
+        return args -> {
+            log.info("Data set oluşturulmuştur");
+            System.out.println("Data set oluşturulmuştur");
+        };
+    }
+}
