98071050090f28ae561e3df48cecf0ed81b7ddac
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-08-21 10:44:05 +0300

    Initial commit

---
+++ src/main/java/com/graysan/TechCareerFullStack2Application.java
@@ -0,0 +1,13 @@
+package com.graysan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCareerFullStack2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechCareerFullStack2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/graysan/TechCareerFullStack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graysan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCareerFullStack2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
