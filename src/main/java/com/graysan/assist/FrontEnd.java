3a372cd5dbe3b6e48b7525e9f74cbff56da79b70
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-08-21 12:42:21 +0300

    Revert "HTTP Exception, CustomErrorHandleWebRequest, ApiResult classes added"
    This reverts commit a34097572060d5b1d51b127b39b7677be49d2c6d.

--- src/main/java/com/graysan/assist/FrontEnd.java
+++
@@ -1,5 +0,0 @@
-package com.graysan.assist;
-
-public class FrontEnd {
-    public static final String REACT_URL = "http://localhost:3000";
-}
--- src/main/java/com/graysan/exception/GraysanException.java
+++
@@ -1,8 +0,0 @@
-package com.graysan.exception;
-
-public class GraysanException extends RuntimeException {
-
-    public GraysanException(String message){
-        super(message);
-    }
-}
